ecd9df8f6b5fb256685de40f9abec0bb0c466070
vietanh021196 <buivietanh021196@gmail.com>
2019-04-18 22:18:45 +0700

    .

---
+++ session1/index.js
@@ -0,0 +1,57 @@
+//const name = "Duc"; chỉ dùng để sử dụng chứ không thay đổi như let
+// let name = "Duc"; //let: kiểu của biến
+// name = "Quy";
+// name = "Viet Anh";
+// console.log("Hello", name);
+//let name = prompt("Enter your name ?"); //nhập vào từ bàn phím
+//console.log("Hello", name);
+
+// let x = prompt ("Enter x"); // or use const
+// let y = prompt ("Enter y"); //or use const
+// console.log(Number(x) + Number(y)); // in ra console
+
+
+// 1. Tạo file html
+// 2. gõ html, chọn html:5 để hiển thị ra khung
+// 3. tại file js để code js
+// 4. Nối 2 file vs nhau bằng cách dùng thẻ <script src="index.js"></script>
+// 5. chạy file html
+// 6. Inspect (ktra) trên trình duyệt (chrom, cococ,...)
+// 7. chuyển qua table console để xem kết quả.
+
+//vẽ hình có màu
+
+//color("green")
+//for (let i = 0; i < 3; i++) {
+//fd(100)
+//rt(90)
+//fd(100)
+//rt(90)
+//fd(100)
+//rt(90)
+//fd(100)
+//rt(90)
+
+//rt(60)
+
+//}
+
+ //vẽ hình vuông
+
+//fd(100)
+//rt(90)
+//fd(100)
+//rt(90)
+//fd(100)
+//rt(90)
+//fd(100)
+//rt(90)
+
+//rt(60)
+
+//loops: lặp
+
+for (let i = 0; i < 10; i++) { //++ là tăng i lên
+console.log(i);
+console.log("Hello")
+}
